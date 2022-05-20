bb652aa6c340676207c5fbc6f3d56d1c985aa2e7
pokerdio <diodor.bitan@gmail.com>
2022-05-20 17:41:18 +0300

    bla blah

---
+++ Main2.java
@@ -0,0 +1,25 @@
+// test java file #1
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] arg) {
+        ArrayList<Thing> things = new ArrayList<Thing>();
+        things.add(new Thing(1,222));
+        things.add(new Thing(6,12));
+        things.add(new Thing(3,98));
+        Collections.sort(things);
+        System.out.println(String.format("%d %d %d", things.get(0).x, 
+                                         things.get(1).x, things.get(2).x));
+
+        Comparator<Thing> c = (Thing t1, Thing t2) -> t1.x - t2.x;
+        Collections.sort(things, c);
+        System.out.println(String.format("%d %d %d", things.get(0).x, 
+                                         things.get(1).x, things.get(2).x));
+
+
+        Collections.sort(things, (Thing t1, Thing t2) -> t1.y - t2.y);
+        System.out.println(String.format("%d %d %d", things.get(0).x, 
+                                         things.get(1).x, things.get(2).x));
+
+    }
+}
---
+++ Thing.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+class Thing{
+    public int x, y; 
+    public Thing (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
