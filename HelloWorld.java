5838bce57c0968ca4c9f0a4439d7206f8501003d
pokerdio <diodor.bitan@gmail.com>
2022-06-13 01:01:37 +0300

    deleted more irrelevant stuffs

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-class HelloWorld {
-    public static void main (String[] args) {
-        System.out.println("Hello World!");
-        MyList foo = new MyList(10);
-        System.out.println(foo.toString());
-    }
-}
